97c500483974c530c0aaf80d1e8865047dfea67f
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-04-04 02:29:32 -0300

    atualização

---
+++ daily-flix-aluraimers-odevclarissa/README.markdown
@@ -0,0 +1,5 @@
+# Daily-flix (Alura - Imersãodev- Clarissa)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/clarcolaco/pen/QWdKZRb](https://codepen.io/clarcolaco/pen/QWdKZRb).
+
+
---
+++ daily-flix-aluraimers-odevclarissa/dist/script.js
@@ -0,0 +1,26 @@
+/* 
+Codigo da aula passada! 
+
+var filmes = ["https://m.media-amazon.com/images/M/MV5BODNiODVmYjItM2MyMC00ZWQyLTgyMGYtNzJjMmVmZTY2OTJjXkEyXkFqcGdeQXVyNzk3NDUzNTc@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BYjhmMGMxZDYtMTkyNy00YWVmLTgyYmUtYTU3ZjcwNTBjN2I1XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+ for (var i = 0; i < filmes.length; i++) {
+ document.write( "<img src=" + filmes[i]+ ">") 
+} 
+
+function adicionarFilme() {
+  console.log("clicou")
+}*/
+
+
+function adicionarFilme() {
+  var campoFilmeFavorito = document.querySelector('#filme')
+   var filme = campoFilmeFavorito.value
+   listarFilmesNaTela(filme)
+  campoFilmeFavorito.value = ""
+}
+
+function listarFilmesNaTela(filme) {
+  var listaFilmes = document.querySelector('#listaFilmes')
+  var elementoFilme = "<img src=" + filme + ">"
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
+}
